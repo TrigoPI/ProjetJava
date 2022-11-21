6b9018af24f0f505b2bd21f54b33242240dbdd20
trigopi <payetalexisg@orange.fr>
2022-11-21 10:41:24 +0200

    AAAAHHHH

--- Implementation/src/main/java/Application/ClavarChatApplication.java
+++ Implementation/src/main/java/Application/ClavarChatApplication.java
@@ -1,14 +1,14 @@
 package Application;
 
 import Application.CLI.CLI;
-import Application.GUI.ClavarChatGUI;
+//import Application.GUI.ClavarChatGUI;
 
 public class ClavarChatApplication
 {
 
     public static void main(String[] args)
     {
-//        CLI.createCLI();
-        new ClavarChatGUI().run();
+        CLI.createCLI();
+//        new ClavarChatGUI().run();
     }
 }
