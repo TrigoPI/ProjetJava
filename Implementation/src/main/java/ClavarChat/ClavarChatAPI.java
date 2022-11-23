bc51878a3f7bdfafba5afe1dce8944bf6f31d46c
trigopi <payetalexisg@orange.fr>
2022-11-23 11:33:09 +0200

    API send

--- Implementation/src/main/java/ClavarChat/ClavarChatAPI.java
+++ Implementation/src/main/java/ClavarChat/ClavarChatAPI.java
@@ -3,11 +3,15 @@ package ClavarChat;
 import ClavarChat.Controllers.Managers.EventManager;
 import ClavarChat.Controllers.Listenner.Listener;
 import ClavarChat.Controllers.Managers.NetworkManager;
+import ClavarChat.Controllers.Managers.UserManager;
 import ClavarChat.Models.ClavarChatMessage.ClavarChatMessage;
+import ClavarChat.Models.ClavarChatMessage.DataMessage;
 import ClavarChat.Models.ClavarChatMessage.Enums.MESSAGE_TYPE;
+import ClavarChat.Models.ClavarChatMessage.TextMessage;
 import ClavarChat.Models.Events.Enums.EVENT_TYPE;
 import ClavarChat.Models.Events.Event;
 import ClavarChat.Models.Events.NetworkMessageEvent;
+import ClavarChat.Models.Users.UserData;
 import ClavarChat.Utils.CLI.CLI;
 import ClavarChat.Utils.CLI.Modules.ModuleCLI;
 import ClavarChat.Utils.Log.Log;
@@ -19,10 +23,12 @@ public class ClavarChatAPI implements Listener
 {
     private EventManager eventManager;
     private NetworkManager networkManager;
+    private UserManager userManager;
 
     public ClavarChatAPI(int tcpPort, int udpPort)
     {
         this.eventManager = EventManager.getInstance();
+        this.userManager = new UserManager();
         this.networkManager = new NetworkManager(tcpPort, udpPort);
 
         this.eventManager.addEvent(EVENT_TYPE.NETWORK_MESSAGE_EVENT);
@@ -40,7 +46,27 @@ public class ClavarChatAPI implements Listener
             for (String ip : ips) this.networkManager.sendUDP(new ClavarChatMessage(MESSAGE_TYPE.DISCOVER), ip);
         });
 
+        moduleCLI.addCommand("send", () -> {
+            String ip = moduleCLI.getUserInput("IP : ");
+            String message = moduleCLI.getUserInput("Message : ");
+            this.sendMessage(message, ip);
+        });
+
         CLI.installModule("api", moduleCLI);
+    }
+
+    public void sendMessage(String message, String ip)
+    {
+        if (this.userManager.isLogged())
+        {
+            UserData user = this.userManager.getUser();
+            TextMessage mgs = new TextMessage(user, message);
+            this.networkManager.sendTCP(message, ip);
+        }
+        else
+        {
+            Log.Warning("User not logged");
+        }
     }
 
     @Override
@@ -61,18 +87,31 @@ public class ClavarChatAPI implements Listener
         switch (data.type)
         {
             case DISCOVER:
-                this.onDiscover(event);
+                this.onDiscover(data, event.src);
+                break;
+            case DATA:
+                this.onData((DataMessage)data, event.src);
                 break;
         }
     }
 
-    private void onDiscover(NetworkMessageEvent event)
+    private void onDiscover(ClavarChatMessage data, String src)
     {
-        Log.Print("Discover from : " + event.src);
+        Log.Print("Discover from : " + src);
     }
 
-//    private boolean isDecodable(Serializable message)
-//    {
-//
-//    }
+    private void onData(DataMessage data, String src)
+    {
+        switch (data.dataType)
+        {
+            case TEXT:
+                this.onTextMessage((TextMessage)data, src);
+                break;
+        }
+    }
+
+    private void onTextMessage(TextMessage data, String src)
+    {
+        Log.Print("Message from " + src + " : " + data.message);
+    }
 }
--- Implementation/src/main/java/ClavarChat/Controllers/Managers/UserManager.java
+++ Implementation/src/main/java/ClavarChat/Controllers/Managers/UserManager.java
@@ -1,9 +1,55 @@
 package ClavarChat.Controllers.Managers;
 
+import ClavarChat.Models.Users.UserData;
+import ClavarChat.Utils.CLI.CLI;
+import ClavarChat.Utils.CLI.Modules.ModuleCLI;
+
 public class UserManager
 {
+    private boolean logged;
+    private UserData user;
+
     public UserManager()
     {
+        this.user = new UserData("", "");
+        this.logged = false;
 
+        this.DEBUG();
+    }
+
+    private void DEBUG()
+    {
+        ModuleCLI moduleCLI = new ModuleCLI();
+
+        moduleCLI.addCommand("set-user", () -> {
+            String pseudo = moduleCLI.getUserInput("Pseudo : ");
+            String id = moduleCLI.getUserInput("id : ");
+
+            this.setUser(pseudo, id);
+        });
+
+        moduleCLI.addCommand("get-user", () -> {
+            System.out.println("User : " + this.user.pseudo);
+            System.out.println("ID : " + this.user.id);
+        });
+
+        CLI.installModule("user", moduleCLI);
+    }
+
+    public boolean isLogged()
+    {
+        return this.logged;
+    }
+
+    public UserData getUser()
+    {
+        this.logged = true;
+        return this.user;
+    }
+
+    public void setUser(String pseudo, String id)
+    {
+        this.user.pseudo = pseudo;
+        this.user.id = id;
     }
 }
