174a5d5f99e6aff41c6f19a9f20ae45e303c0547
Payet Alexis <56426746+TrigoPI@users.noreply.github.com>
2023-01-11 20:19:14 +0200

    Delete build.cmd

--- Implementation/build.cmd
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-mkdir dist
-mkdir .\dist\Resources
-call .\build\maven\bin\mvn assembly:assembly
-copy .\target\ClavarChat-1.0-SNAPSHOT-jar-with-dependencies.jar .\dist
-C:\Users\payet\.jdks\openjdk-19.0.1\bin\java.exe -jar .\dist\ClavarChat-1.0-SNAPSHOT-jar-with-dependencies.jar
