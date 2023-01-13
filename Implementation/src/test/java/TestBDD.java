067032813fa404c102559ac3daf43598525886c9
clementroussel <caa.roussel@gmail.com>
2023-01-13 13:43:38 +0100

    test BDD fini mais ne marche pas sur mon Mac

--- Implementation/src/test/java/TestBDD.java
+++ Implementation/src/test/java/TestBDD.java
@@ -2,15 +2,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import ClavarChat.Controllers.API.DataBaseAPI;
 import ClavarChat.Controllers.Managers.User.UserManager;
 import Resources.Resources;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
 class TestBDD {
-
     private UserManager uManager = new UserManager();
     private DataBaseAPI api = new DataBaseAPI(uManager);
 
+    @Test
+    @DisplayName("Test pour verifier l'insertion d'un user et la modification de son pseudo")
     void testUserPseudonym() throws IOException {
         this.api.clear(); //clear la BDD pour qu'il n'y ait pas de résidu
         //test insert user
@@ -42,5 +45,44 @@ class TestBDD {
         assertEquals(user3,this.api.getUserPseudo(userid3));
 
         this.api.clear();
+    }
+    @Test
+    @DisplayName("Test pour verifier l'insertion d'utilisateur dans la BDD et l'update d'avatar")
+    void testUserAvatar() throws IOException {
+        this.api.clear();
+        String user1="USER1";
+        int userid1 = 1111;
+        byte[] buffer = Resources.IMG.USER_1.readAllBytes();
+        this.api.addUser(userid1,user1,buffer);
+        //verifie si la longueur est identique pour ne pas avoir de segmentation fault
+        if (buffer.length == this.api.getUserAvatar(userid1).length) {
+            for (int i=0; i<buffer.length;i++){
+                //test chaque byte de l'image et la compare avec celle d'origine
+                assertEquals(buffer[i],this.api.getUserAvatar(userid1)[i]);
+            }
+        }
+        buffer=Resources.IMG.LOGO.readAllBytes();
+        this.api.updateAvatar(userid1,buffer);
+        //verifie si la longueur est identique pour ne pas avoir de segmentation fault
+        if (buffer.length == this.api.getUserAvatar(userid1).length) {
+            for (int i = 0; i < buffer.length; i++) {
+                //test chaque byte de l'image et la compare avec celle d'origine
+                assertEquals(buffer[i], this.api.getUserAvatar(userid1)[i]);
+            }
+        }
+        this.api.clear();
+    }
+
+    @Test
+    @DisplayName("test sur l'ajout de message à la BDD")
+    void testMessage() {
+        String message = "testMESSAGE";
+        int convID=1;
+        int senderID = 1111;
+        int receiverID=2222;
+        this.api.addMessage(convID,senderID,receiverID,message);
+        assertEquals(message,this.api.getMessageText(this.api.getMessagesId(convID).get(0)));
+        //pas besoin de tester l'id vu qu'il est créé par autoincrémantation
+        //pas d'update à tester
     }
 }
